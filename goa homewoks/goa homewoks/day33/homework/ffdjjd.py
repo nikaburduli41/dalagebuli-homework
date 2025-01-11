c0252ddabf96231509663be3cd85e2caa32ae27a
nikaburduli41 <burdulinika22@gmail.com>
2025-01-11 22:08:38 +0400

    Update ffdjjd.py

--- goa homewoks/goa homewoks/day33/homework/ffdjjd.py
+++ goa homewoks/goa homewoks/day33/homework/ffdjjd.py
@@ -14,6 +14,7 @@ def solution(nums):
 def capitals(word):
     return [index for index, char in enumerate(word) if char.isupper()]
 
+#6
 def manual_join(iterable, delimiter=" "):
     result = ""
     for i, element in enumerate(iterable):
@@ -21,3 +22,19 @@ def manual_join(iterable, delimiter=" "):
             result += delimiter
         result += element
     return result
+
+#5
+def sum_cubes(n):
+    return sum( i**3 for i in range(n+1) )
+
+
+#7
+def max_multiple(divisor, bound):
+    return bound - (bound % divisor)
+
+#8
+def sum_digits(number):
+    return sum(int(digit) for digit in str(abs(number)))
+
+def dont_give_me_five(start,end):
+    return sum('5' not in str(num) for num in range(start, end + 1))
