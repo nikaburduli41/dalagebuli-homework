d8f20e360ed51ac0e52168e62756c23ec1a3f10c
nikaburduli41 <burdulinika22@gmail.com>
2025-01-12 21:37:27 +0400

    Add files via upload

---
+++ goa homewoks/goa homewoks/day 35/classwork/classwork.py
@@ -0,0 +1,12 @@
+def to_jaden_case(string):
+    return ' '.join(word.capitalize() for word in string.split())
+
+def sequence_sum(start, end, step):
+    return sum(range(start, end+1, step))
+
+def sequence_sum(start, end, step):
+    return sum(range(start, end+1, step))
+
+def lowercase_count(strng):
+    return sum(a.islower() for a in strng)
+
