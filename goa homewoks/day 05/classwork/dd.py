54d99631395508c50cd6d6474ccfc45405393462
nikaburduli41 <burdulinika22@gmail.com>
2025-02-22 14:01:23 +0400

    Add files via upload

---
+++ goa homewoks/day 05/classwork/dd.py
@@ -0,0 +1,4 @@
+name = input("please enter your name: ")
+surrname = input("please enter your surrname: ")
+age = input("please enter your age: ")
+print("Hello", name, surrname, "you are", age, "years old")
