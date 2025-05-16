e4a2842c92c75285d71f63605b7fa3272d5af6c8
nikaburduli41 <burdulinika22@gmail.com>
2025-05-16 10:37:50 +0400

    Add files via upload

---
+++ goa homewoks/day 72/classwork/javascript.js
@@ -0,0 +1,19 @@
+//1//
+console.log(math.max(...(10,11,11,49,399)))
+
+console.log(math.min(...(10,11,11,49,399)))
+
+//6//
+let info3 = 10.5
+console.log(math.round(info3))
+
+//2//
+console.log(math.random * 10)
+
+//5//
+let info4 = 10
+console.log(math.pow(info4, 4))
+
+//3//
+let info5 = -10
+console.log(math.abs(info5))
