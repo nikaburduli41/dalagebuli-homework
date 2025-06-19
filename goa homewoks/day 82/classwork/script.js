cc07fb8469f168e5d87a4ad9567c234ada2bef5b
nikaburduli41 <burdulinika22@gmail.com>
2025-06-19 21:20:05 +0400

    Add files via upload

---
+++ goa homewoks/day 82/classwork/script.js
@@ -0,0 +1,21 @@
+//1//
+let num = prompt("enter num")
+while (num < 0){
+    console.log(num)
+    num++
+}
+//2//
+let nam = prompt("enter num")
+while (nam.length < 10)
+    console.log(nam)
+    nam++
+//3//
+let pin = prompt("enter pincode")
+while (pin !== 6446){
+    prompt("enter pin")
+}
+//4//
+let saxeli = prompt("enter name")
+while (saxeli !== "vanochka"){
+    prompt("enter name")
+}
