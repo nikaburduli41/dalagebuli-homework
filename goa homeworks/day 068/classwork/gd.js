d098eec9d772cc729dcbbc9081e3144d948c9963
nikaburduli41 <burdulinika22@gmail.com>
2025-11-08 01:25:03 +0400

    Add files via upload

---
+++ goa homeworks/day 068/classwork/gd.js
@@ -0,0 +1,20 @@
+//+ პირველ რიცხვს ამატებს მეორე რიცხვს - პირველ რიცხვს აკლებს მეორე რიცხვს * ერთ რიცხვს ამრავლებს მეორე რიცხვზე / ყოფს ერთ რიცხვს მეორეზე
+console.log(1 + 2);
+console.log(1 + 3);
+console.log(1 + 4);
+console.log(90 - 5);
+console.log(90 - 6);
+console.log(90 - 7);
+console.log(90 * 8);
+console.log(90 * 9);
+console.log(90 * 10);
+console.log(90 / 10);
+console.log(90 / 9);
+console.log(90 / 45);
+
+//ეს ტქესტი გადმოიტანეთ JavaScript'ში და შეინახეთ ცვლადშ ბოლოს კი გამოიტანეთ ეს ცვლადი 
+let x = getElementById("p1099").indehtml = "edited in emv";
+console.log(x)
+
+let y = 4
+console.log(++y)
---
+++ goa homeworks/day 068/homework/script.js
@@ -0,0 +1,16 @@
+
+//let segilia secvalo cvladebis monacemebi da eseni magram
+//const ar segvilia sevcvalot
+
+let x = 10;
+
+{
+  let x = 20;}
+console.log(x);
+
+
+let y = 5 ;
+console.log(y++);
+//5
+console.log(++y);
+//6
