180166d8cc1e53dcf4b521086b7e0634916972b7
nikaburduli41 <burdulinika22@gmail.com>
2026-02-06 20:31:32 +0400

    Add files via upload

---
+++ goa homeworks/day 143/homework/script.js
@@ -0,0 +1,24 @@
+//1//
+let list = []
+let last = []
+let str = 'sff fda fas'
+list.push(str.split(' '))
+if(list.includes('A','a')){
+  last.push(list)
+  
+}
+console.log(last)
+//2//
+let users = {
+    name:'nika',
+    age:30
+}
+if (users.age >= 30){
+    console.log(users.age)
+}
+//3//
+let words = ['wed','dffd','dfdfdf']
+let liste = []
+if(words.length > 5){
+    words.push(liste)
+}
