ee352ae8742dcee04f476507f27a637d19c1ccee
nikaburduli41 <burdulinika22@gmail.com>
2026-03-29 15:09:34 +0400

    Add files via upload

---
+++ goa homeworks/day 155/classwork/src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+
+const root1 = document.getElementById("root");
+const root = ReactDOM.createRoot(root1);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
